adbb2a756c7abd4b366e35d12cdf7c3bc7c62c2b
kazutvoice05 <kazutvoice05@gmail.com>
2022-06-03 01:18:09 +0900

    firebase導入

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,41 @@
     </div>
     <button class="bg-blue-500" v-on:click="$store.dispatch('updatePostAction')">GetPost</button>
     <Tutorial/>
+    <div>
+        {{ message }}
+    </div>
+    <button v-on:click="getFirebase">AccessFirebase</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup() {
+  },
+  data() {
+    return {
+      message: "asdfdsa"
+    }
+  },
+  methods: {
+    async getFirebase() {
+      try {
+        const query = await this.$fire.firestore.collection('data').get()
+
+        const docs = []
+        query.forEach((doc) => {
+          var data = doc.data()
+          var record = [doc.id, data.name, data.user_id]
+          console.log(record)
+          docs.push(record)
+        })
+
+        this.message = docs
+      } catch (err) {
+        console.log('failed to fetch data from firebase.')
+      }
+    }
+  }
 }
 </script>
